71dcfd03de5843aeb4430e457ef283d157527a73
justisGipson <justis710@gmail.com>
2019-02-07 12:34:28 -0500

    proj start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeAndNavComponent } from './home-and-nav/home-and-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeAndNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-and-nav/home-and-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-and-nav',
+  templateUrl: './home-and-nav.component.html',
+  styleUrls: ['./home-and-nav.component.css']
+})
+export class HomeAndNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
